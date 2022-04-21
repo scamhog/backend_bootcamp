dc496f5f82d6fa18720da73a3c90c57fce18c7f6
unknown <marcocastrosendoya@outlook.com>
2022-04-21 15:25:46 -0400

    fibonacci with yield function generator

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,25 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+// import express, { Application, Request, Response, NextFunction } from 'express';
 
-const app: Application = express();
-const $PORT = 3000;
+// const app: Application = express();
+// const $PORT = 3000;
 
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Hello World!');
-});
+// app.get('/', (req: Request, res: Response, next: NextFunction) => {
+//     console.log(req.ip);
+//     res.send('Hello World!');
+// });
 
-app.listen($PORT, () => console.log(`Server is running on port: ${$PORT}`))
+// app.listen($PORT, () => console.log(`Server is running on port: ${$PORT}`));
+
+// PLAY SANDBOX
+function* fibonacci(){
+    let tuple = [1,1];
+    while(true){
+        let n = 0;
+        console.log(n = tuple.reduce((n,m)=>{return n+m}));
+        yield tuple = [tuple[1], n];
+    }
+}
+
+let sequence = fibonacci();
+for (let index = 0; index < 10; index++)
+    sequence.next();
